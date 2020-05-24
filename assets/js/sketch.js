f881367919f2a9b723042cd7e56e010276504ad4
imlolman <satyam.gupta756@gmail.com>
2020-05-24 13:19:27 +0530

    Added UI, Functionality and AI Remaining

---
+++ assets/js/sketch.js
@@ -0,0 +1,17 @@
+var HEIGHT = 8
+var WIDTH = 8
+var MINES = 8
+
+function runGame(){
+    document.querySelector('.withoutGame').style.display = "none";
+    document.querySelector('.withGame').style.display = "flex";
+    resetGame()
+}
+
+function resetGame(){
+
+}
+
+function aiMove(){
+
+}
--- readme.md
+++ readme.md
@@ -5,4 +5,3 @@ Check it out on: https://imlolman.github.io/Minesweeper-AI/
 YT Link: https://youtu.be/Nx90Vu3xOAA
 
 My Site: https://imlolman.github.io/
-Check Last Commit to get Modifications done to the base Code.
